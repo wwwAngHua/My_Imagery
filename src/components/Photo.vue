39eee781bebbd1e74869fda8e0c7723688cc739f
王华 <wanghua@MacBook-Pro.local>
2024-03-16 12:03:59 +0800

    commit

--- src/components/Photo.vue
+++ src/components/Photo.vue
@@ -1,13 +1,45 @@
 <script setup lang="ts">
-const photos = [
-    'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-    'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-    'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-    'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-    'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-    'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-    'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-]
+import { ref } from 'vue'
+interface Photo {
+    url: string
+    title: string
+}
+
+const photos = ref<Array<Photo>>([
+    {
+        url: 'https://img.t1ykf.com/file/f7b19f972774213dabff5.jpg',
+        title: 'Photo 1',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/b8286d1d920f2843d794f.jpg',
+        title: 'Photo 2',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/c665df4d4f90aca9fbf4a.jpg',
+        title: 'Photo 3',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/1f9598fa187c42868aadc.jpg',
+        title: 'Photo 4',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/86bc8c088219abf08a6d3.jpg',
+        title: 'Photo 5',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/624f259693157e5f21ee6.jpg',
+        title: 'Photo 6',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/6aca947249ac58c2c43e3.jpg',
+        title: 'Photo 7',
+    },
+    {
+        url: 'https://img.t1ykf.com/file/c13bad6d39bd4fda6a503.jpg',
+        title: 'Photo 8',
+    },
+])
+const photoUrls = photos.value.map((photo) => photo.url)
 </script>
 
 <template>
@@ -21,19 +53,19 @@ const photos = [
                 :lg="6"
                 :xl="6"
                 lazy
-                v-for="(src, index) in photos"
+                v-for="(data, index) in photos"
                 :key="index">
                 <el-image
                     style="width: auto; height: auto"
-                    :src="src"
+                    :src="data.url"
                     :zoom-rate="1.2"
                     :max-scale="7"
                     :min-scale="0.2"
-                    :preview-src-list="photos"
+                    :preview-src-list="photoUrls"
                     :initial-index="index"
-                    fit="contain" /><el-text class="mx-1" size="small"
-                    >Small</el-text
-                ></el-col
+                    fit="contain" /><el-text class="mx-1" size="small">{{
+                    data.title
+                }}</el-text></el-col
             ></el-row
         >
     </div>
